8eecabb2e4e91e07c3a5cdf5931b7d13c4497bdd
Petrus <petrus_ant@hotmail.com>
2021-06-19 21:52:28 -0300

    [Mixin-EnderIO] Cancel travel only if target location is more than 350 blocks away from the origin

--- Mixin-EnderIO/src/main/java/io/github/crucible/grimoire/mixins/enderio/MixinPacketTravelEvent.java
+++ Mixin-EnderIO/src/main/java/io/github/crucible/grimoire/mixins/enderio/MixinPacketTravelEvent.java
@@ -3,19 +3,30 @@ package io.github.crucible.grimoire.mixins.enderio;
 import crazypants.enderio.api.teleport.TravelSource;
 import crazypants.enderio.teleport.packet.PacketTravelEvent;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = PacketTravelEvent.class, remap = false)
 public abstract class MixinPacketTravelEvent {
 
     /**
-     * @author juanmuscaria
-     * @reason Desativar o pacote temporariamente em quanto não tem uma correção definitiva.
+     * @author evernife
+     * @reason Disable teleport if entity is more than 250 blocks away from the target
      */
-    @Overwrite
-    public static boolean doServerTeleport(Entity toTp, int x, int y, int z, int powerUse, boolean conserveMotion, TravelSource source) {
-        return false;
-        //Vai ficar desativado até botar tudo no server side, caso não tiver queda de desempenho será reativado.
+    @Inject(method = "doServerTeleport", at = @At("HEAD"), cancellable = true)
+    private static void doServerTeleport(Entity toTp, int x, int y, int z, int powerUse, boolean conserveMotion, TravelSource source, CallbackInfoReturnable<Boolean> cir) {
+        int xOrigin = MathHelper.floor_double(toTp.posX);
+        int zOrigin = MathHelper.floor_double(toTp.posZ);
+
+        int squareDistance = (z - zOrigin) * (z - zOrigin) + (x - xOrigin) * (x - xOrigin);
+
+        if (squareDistance >= 122500){ //sqrt(122500) == 350
+            cir.setReturnValue(true);
+            return;
+        }
     }
+
 }
